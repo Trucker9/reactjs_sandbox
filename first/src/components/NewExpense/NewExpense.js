d1fcbb6228183cdf906618d08bbdd0bad64195ab
puzzlestudio1 <ali.fam0079@gmail.com>
2022-03-24 11:41:20 +0430

    (qf) passing dara from child to parent element

--- first/src/components/NewExpense/NewExpense.js
+++ first/src/components/NewExpense/NewExpense.js
@@ -5,7 +5,7 @@ import './NewExpense.css';
 
 const NewExpense = (props) => {
   // Trying to send data from child to parent, they way it works is that we send a custom function as props to child element 
-  // Then we call that function whenever we want.
+  // Then we call that function whenever we want in the child element. REMEMBER: no child or parent can be skipped. this must be done level by level
 
   const saveExpenseDataHandler = (enteredExpenseData)=>{
     const toSaveData = {
