0ccd345877f619e95993d8a36a5d1800e43298ec
puzzlestudio1 <ali.fam0079@gmail.com>
2022-03-26 23:51:42 +0430

    second

--- first/src/components/UI/Card.js
+++ first/src/components/UI/Card.js
@@ -5,7 +5,9 @@ import './Card.css';
 const Card = (props) => {
   // classNames that we pass to <Card /> will be available here. then we set them to the wrapper div.
   const classes = 'card ' + props.className;
+  console.log(classes);
   // everything that is between <Card> <Card/> will be replaced with props.children
+  // Basically we are wrapping all the content into "div" because we cannot use custom components at the top level like what we did at App.js with <Card>
   return <div className={classes}> {props.children} </div>;
 };
 
