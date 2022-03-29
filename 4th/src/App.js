c9d01ee6e4bba132e4c4d3da9118d28764435996
puzzlestudio1 <ali.fam0079@gmail.com>
2022-03-29 23:32:07 +0430

    useEffect (run once)

--- 4th/src/App.js
+++ 4th/src/App.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
+  // useEffect Usage 1: run one time
+  // function of the useEffect only runs once, when this component is being created.
+  // later when the state changed with  setIsLoggedIn(false); useEffect will run again, but since there is no dependencies to change,
+  // its function wont run again.
+  const useEffect_function = () => {
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInfo === "1") setIsLoggedIn(true);
   };
+  useEffect(useEffect_function, []);
 
+  const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', '1');
+    
+    // This state changing tells react to change the webpage.
+    setIsLoggedIn(true);
+  }
   const logoutHandler = () => {
     setIsLoggedIn(false);
   };
