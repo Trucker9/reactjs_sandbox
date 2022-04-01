4c50a8516ed0cd71714146f52ae8181dd16bb2ab
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-01 16:55:09 +0430

    useCallback with memo

--- 5th/src/App.js
+++ 5th/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -9,10 +9,19 @@ function App() {
 
   console.log('APP RUNNING');
 
-  const toggleParagraphHandler = () => {
+  /* To prevent what happened in the last commit, we have useCallback.
+  we can store a function of a component in some place of React memory, and when ever the component runs again, React
+  does not create a new function for that. instead uses that saved function in the memory from the past.
+  now when Reacts compares toggleParagraphHandlers from last run and current run, they are the same. so nothing changes
+  from Button component and it wont get re evaluated because it has React.memo()
+  useCallback gets another argument, array of dependencies similar to useEffect.
+   with it being empty, we tell React that the toggleParagraphHandler
+  never will change and always use what you've saved in memory.
+  */
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
- 
+  });
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
@@ -24,7 +33,7 @@ function App() {
       useless behavior of React, we use memo.
       We apply this on DemoOutput*/}
       <DemoOutput show={false} />
-      
+
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
