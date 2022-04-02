51991bc00047609fbb711700a572d81c248fc1af
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-02 22:58:29 +0430

    why custom hooks?

--- 7th/src/App.js
+++ 7th/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import BackwardCounter from './components/BackwardCounter';
 import ForwardCounter from './components/ForwardCounter';
 
+// Why we need custom hooks ?
+// We can't use React hooks in any function. we can use them in component functions and custom hooks
+// here we want to refactor Forward and Backward into one function but both of them use hooks.
+// We have to use custom hooks to do that.
 function App() {
   return (
     <React.Fragment>
