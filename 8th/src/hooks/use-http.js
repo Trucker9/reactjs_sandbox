7c10ae383620483cb7e7668cd6538a7f40f3d8bf
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-03 03:38:09 +0430

    building http custom hook

---
+++ 8th/src/hooks/use-http.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+const useHttp = (reqCfg, handleData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendReq = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(reqCfg.url, {
+        method: reqCfg.method,
+        headers: reqCfg.headers,
+        body: JSON.stringify(reqCfg.body),
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      // Here we do whatever the fuck we want to do with the data.
+      handleData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+
+  // We need these back at the component that used "useHttp" so we are returning them
+  return {isLoading, error, sendReq}
+};
+
+export default useHttp;
