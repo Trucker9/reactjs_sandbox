0feb2309746e19c7fa53567278813d31a9f98a53
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-02 14:05:12 +0430

    using Async

--- 6th/src/App.js
+++ 6th/src/App.js
@@ -5,24 +5,20 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const fetchMoviesHandler = () => {
-    const movies = fetch('https://swapi.dev/api/films')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        // Transforming API data to match our code.
-        const transformedMovies = data.results.map((el) => {
-          return {
-            id: el.episode_id,
-            title: el.title,
-            openingText: el.opening_crawl,
-            releaseDate: el.release_date,
-          };
-        });
-        // Changing state.
-        setMovies(transformedMovies);
-      });
+  const fetchMoviesHandler = async () => {
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+    // Transforming API data to match our code.
+    const transformedMovies = data.results.map((el) => {
+      return {
+        id: el.episode_id,
+        title: el.title,
+        openingText: el.opening_crawl,
+        releaseDate: el.release_date,
+      };
+    });
+    // Changing state.
+    setMovies(transformedMovies);
   };
 
   return (
