087b586c464d95dc6250f7bea80ce026bebdf675
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-04 03:55:47 +0430

    overall form validity

--- 9th/src/components/SimpleInput.js
+++ 9th/src/components/SimpleInput.js
@@ -1,12 +1,27 @@
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-
   // We simply check for these two.
   const name_IsValid = enteredName.trim() !== '';
   const name_touchedInvalid = !name_IsValid && enteredNameTouched;
+
+  // Let's say we have more fields in our form rather than just name.
+  // We can check for our form validity by checking all the input fields validity with useEffect like this:
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  // function of the useEffect runs for the first time that component is being rendering and each time a validity changes.
+  useEffect(
+    () => {
+      // If we had more input fields in our form, we would've added them here.
+      if (name_IsValid) {
+        setFormIsValid(true);
+      } else setFormIsValid(false);
+    },
+    // If we had more input fields in our form, we would've added them here.
+    [name_IsValid]
+  );
 
   const nameInChangeHandler = (e) => {
     // Re evaluate in every key stroke.
@@ -29,7 +44,7 @@ const SimpleInput = (props) => {
   const formClass = name_touchedInvalid
     ? 'form-control invalid'
     : 'form-control';
-    
+
   return (
     <form>
       <div className={formClass}>
@@ -46,7 +61,9 @@ const SimpleInput = (props) => {
       </div>
       {name_touchedInvalid ? <p className="error-text">Invalid name</p> : ''}
       <div className="form-actions">
-        <button onClick={formSubmissionHandler}>Submit</button>
+        <button disabled={!formIsValid} onClick={formSubmissionHandler}>
+          Submit
+        </button>
       </div>
     </form>
   );
