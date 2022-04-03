273113b00dcc489ec8d8c451a7bf195d665ffdde
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-03 16:54:57 +0430

    getting input with state

--- 9th/src/components/SimpleInput.js
+++ 9th/src/components/SimpleInput.js
@@ -1,11 +1,23 @@
 import { useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
-  // If we want to use the value once, we can use useRef. 
+  /*   // If we want to use the value once, we can use useRef.
   const nameInRef = useRef();
   const formSubmissionHandler = (e) => {
     e.preventDefault();
     console.log(nameInRef.current.value);
+  };
+*/
+  // For using input with every key stroke for example validation, its better to use state.
+  // We have better control with state
+  const [enteredName, setEnteredName] = useState('');
+  const nameInChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredName);
+    setEnteredName('');
   };
 
   return (
@@ -13,8 +25,13 @@ const SimpleInput = (props) => {
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
         <input
-         ref={nameInRef}
-          type="text" id="name" />
+          //  ref={nameInRef}
+          onChange={nameInChangeHandler}
+          type="text"
+          id="name"
+          // Feeding back
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button onClick={formSubmissionHandler}>Submit</button>
