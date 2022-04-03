869f1f25bdb35ebff270f72654cc9268bf154821
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-03 04:46:23 +0430

    using useHttp for POST

--- 8th/src/components/NewTask/NewTask.js
+++ 8th/src/components/NewTask/NewTask.js
@@ -1,42 +1,32 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendReq: sendTask } = useHttp();
 
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        // Here we have some code duplication.
-        'https://react-course-4b234-default-rtdb.europe-west1.firebasedatabase.app//tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTask(
+      {
+        url: 'https://react-course-4b234-default-rtdb.europe-west1.firebasedatabase.app//tasks.json',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { text: taskText },
+      },
+      // We need taskText in createTask.
+      // We use bind to pre configure a function. 
+      // first, we set the this key word to null because we don't need it.
+      // second, we pass taskText into it. so now when we call create task, what ever we passed into it + this argument will be sent to function
+      createTask.bind(null, taskText)
+    );
   };
 
   return (
