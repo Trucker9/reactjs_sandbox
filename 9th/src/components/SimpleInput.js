3d11c1aa5ca1d512a9eb877d7f78d29d33977c4a
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-04 02:21:56 +0430

    Fixing a problem from last commit

--- 9th/src/components/SimpleInput.js
+++ 9th/src/components/SimpleInput.js
@@ -11,7 +11,14 @@ const SimpleInput = (props) => {
   // For using input with every key stroke for example validation, its better to use state.
   // We have better control with state
   const [enteredName, setEnteredName] = useState('');
-  const [nameIsValid, setNameIsValid] = useState(true);
+  /* This causes a problem. if we use useEffect and check if(nameIsValid) {},
+   at the beginning, although we have no input, we execute the if block!
+   totally its not a good practice to say nameIsValid when its empty (logically) */
+  const [nameIsValid, setNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  
+  // if its touched and is invalid, we show the error message.
+  const nameIsInvalid = !nameIsValid && enteredNameTouched;
 
   const nameInChangeHandler = (e) => {
     setEnteredName(e.target.value);
@@ -19,6 +26,8 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (e) => {
     e.preventDefault();
+
+    setEnteredNameTouched(true);
 
     if (enteredName.trim() === '') {
       setNameIsValid(false);
@@ -35,7 +44,7 @@ const SimpleInput = (props) => {
   return (
     <form>
       {/* If name is invalid we add 'invalid' class.*/}
-      <div className={`form-control ${!nameIsValid && 'invalid'}`}>
+      <div className={`form-control ${nameIsInvalid && 'invalid'}`}>
         <label htmlFor="name">Your Name</label>
         <input
           //  ref={nameInRef}
@@ -46,7 +55,7 @@ const SimpleInput = (props) => {
           value={enteredName}
         />
       </div>
-      {nameIsValid ? '' : <p className="error-text">Invalid name</p>}
+      {!nameIsInvalid ? '' : <p className="error-text">Invalid name</p>}
       <div className="form-actions">
         <button onClick={formSubmissionHandler}>Submit</button>
       </div>
