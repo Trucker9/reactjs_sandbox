2796ed1f08ee3c5632189128cce0d90658e103a3
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-09 00:22:52 +0430

    adding slice to store

--- 10th/src/store/index.js
+++ 10th/src/store/index.js
@@ -1,7 +1,7 @@
 // There is a problem using redux like this.
-// In bigger apps, this file and its duties will get significantly larger and its hard to maintain. 
+// In bigger apps, this file and its duties will get significantly larger and its hard to maintain.
 // So we use redux tool kit
-import {createSlice} from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 import { createStore } from 'redux';
 
 const initialState = {
@@ -10,16 +10,16 @@ const initialState = {
 };
 
 // When we have different states that are not directly related we can use different slices.
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState: initialState,
-  // Here WE CAN mutate our state obj. redux toolkit will take care of bugs for us. 
+  // Here WE CAN mutate our state obj. redux toolkit will take care of bugs for us.
   reducers: {
     increment(state, action) {
       state.counter++;
     },
     decrement(state, action) {
-      state.counter --;
+      state.counter--;
     },
     increase(state, action) {
       state.counter = state.counter + action.by;
@@ -27,31 +27,17 @@ createSlice({
     toggleCounter(state, action) {
       state.showCounter = !state.showCounter;
     },
-  }
+  },
 });
 
-const counterReducer = (existingState = initialState, action) => {
-  if (action.type === 'INCREMENT' && action.by)
-    return {
-      ...existingState,
-      counter: existingState.counter + 5,
-    };
-  if (action.type === 'INCREMENT')
-    return {
-      ...existingState,
-      counter: existingState.counter + 1,
-    };
+// One way of passing reducer methods to store is like this. note that here is called "reducer" and in the createSlice argument is "reducers"
+// const store = createStore(counterSlice.reducer);
 
-  if (action.type === 'DECREMENT')
-    return { ...existingState, counter: existingState.counter - 1 };
-
-  if (action.type === 'TOGGLE') {
-    return { ...existingState, showCounter: !existingState.showCounter };
-  }
-
-  return existingState;
-};
-
-const store = createStore(counterReducer);
+// If we had more than one slice, we have multiple reducer objects to add to store.
+// but note that redux always wants one "reducer" object containing all of our reducer methods.
+// We need to create out store like this, all of reducer objects that we pass as "reducer" object properties, are object containing reducer methods. 
+const store = configureStore({
+  reducer: { counter: counterSlice },
+});
 
 export default store;
