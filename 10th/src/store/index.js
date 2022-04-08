b74612f5503eeb24b630baa311e41a1a3d63d07f
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-09 00:13:01 +0430

    creating slice

--- 10th/src/store/index.js
+++ 10th/src/store/index.js
@@ -1,14 +1,36 @@
 // There is a problem using redux like this.
 // In bigger apps, this file and its duties will get significantly larger and its hard to maintain. 
 // So we use redux tool kit
+import {createSlice} from '@reduxjs/toolkit'
 import { createStore } from 'redux';
 
-const statesObj = {
+const initialState = {
   showCounter: true,
   counter: 0,
 };
 
-const counterReducer = (existingState = statesObj, action) => {
+// When we have different states that are not directly related we can use different slices.
+createSlice({
+  name: 'counter',
+  initialState: initialState,
+  // Here WE CAN mutate our state obj. redux toolkit will take care of bugs for us. 
+  reducers: {
+    increment(state, action) {
+      state.counter++;
+    },
+    decrement(state, action) {
+      state.counter --;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.by;
+    },
+    toggleCounter(state, action) {
+      state.showCounter = !state.showCounter;
+    },
+  }
+});
+
+const counterReducer = (existingState = initialState, action) => {
   if (action.type === 'INCREMENT' && action.by)
     return {
       ...existingState,
