a7c7be6885bd5206d39a511d494ecfa69c94e301
puzzlestudio1 <ali.fam0079@gmail.com>
2022-04-08 17:05:12 +0430

    dispatching actions with buttons

--- 10th/src/components/Counter.js
+++ 10th/src/components/Counter.js
@@ -1,29 +1,39 @@
 // we Extract the data we need from store with useSelector. we dont use the whole store.
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+
   // This function gets the whole state and returns what we are interested in.
   const extractorFn = (state) => {
+ 
     return state.counter;
   };
   // Extracting data from store with useSelector and extractorFn.
   // useSelector automatically subscribes this component to the store.
   // by subscribing, whenever the counter (or generally the data) changes in the redux store, the component will be updated with the latest value.
   // Changes to the redux store will cause this component to run again and show the latest counter.
-  // if we unmount this component, the subscription will be removed automatically. 
+  // if we unmount this component, the subscription will be removed automatically.
   const counterValue = useSelector(extractorFn);
 
   const toggleCounterHandler = () => {};
-
+  const incHandler = () => {
+    const action = { type: 'INCREMENT' };
+    dispatch(action);
+  };
+  const decHandler = () => {
+    const action = { type: 'DECREMENT' };
+    dispatch(action);
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>-- {counterValue} --</div>
       <div>
-        <button>Increment</button>
-        <button>decrement</button>
+        <button onClick={incHandler}>Increment</button>
+        <button onClick={decHandler}>decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- 10th/src/store/index.js
+++ 10th/src/store/index.js
@@ -2,8 +2,10 @@ import { createStore } from 'redux';
 
 // Reducer Function that is responsible for changing store.
 const counterReducer = (existingState = { counter: 0 }, action) => {
-  if (action.type === 'INCREMENT') return { counter: existingState.counter++ };
-  if (action.type === 'DECREMENT') return { counter: existingState.counter-- };
+  if (action.type === 'INCREMENT')
+    return { counter: existingState.counter + 1 };
+  if (action.type === 'DECREMENT')
+    return { counter: existingState.counter - 1 };
   return existingState;
 };
 // Creating store and giving access to its reducer function.
