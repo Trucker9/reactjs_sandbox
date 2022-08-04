84185d359a5f538f96076c8321c368b8c23e5b82
puzzlestudio1 <ali.fam0079@gmail.com>
2022-08-04 20:49:01 +0430

    Adding auth to store

--- 10th/src/store/index.js
+++ 10th/src/store/index.js
@@ -30,19 +30,39 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state){
+      state.isAuthenticated = true;
+    },
+    logout(state){
+      state.isAuthenticated = false;
+    }
+  }
+});
+
 // One way of passing reducer methods to store is like this. note that here is called "reducer" and in the createSlice argument is "reducers"
 // const store = createStore(counterSlice.reducer);
 
 // If we had more than one slice, we have multiple reducer objects to add to store.
 // but note that redux always wants one "reducer" object containing all of our reducer methods.
 // We need to create out store like this, all of reducer objects that we pass as "reducer" object properties, are object containing reducer methods.
-// in chon yeki bood kar nakard. majbooram avazesh konam. inja yedoone reducer darim hamoono midim be store.
+
 const store = configureStore({
-  // reducer: { counter: counterSlice.reducer },
-  reducer: counterSlice.reducer,
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
 // This object contains our reducer methods and we can use them safely.
 export const counterAction = counterSlice.actions;
+export const authAction = authSlice.actions;
 
 export default store;
