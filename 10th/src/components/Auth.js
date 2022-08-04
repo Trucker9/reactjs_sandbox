bc416772e2bff6785e1b70f52d300ffd7263ac09
puzzlestudio1 <ali.fam0079@gmail.com>
2022-08-04 21:15:32 +0430

    using auth slice in components

--- 10th/src/components/Auth.js
+++ 10th/src/components/Auth.js
@@ -1,10 +1,23 @@
+import { useDispatch } from 'react-redux';
+
+import { authAction } from '../store/index';
 import classes from './Auth.module.css';
 
 const Auth = () => {
+
+const dispatch = useDispatch();
+
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    // authAction.login() returns an action and then dispatch sends it to the store to execute the login reducer method
+    dispatch(authAction.login());
+  }
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
--- 10th/src/components/Header.js
+++ 10th/src/components/Header.js
@@ -1,22 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { authAction } from '../store/index';
 import classes from './Header.module.css';
 
 const Header = () => {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const dispatch = useDispatch();
+
+  const logOutHandler = (e) => {
+    e.preventDefault();
+    dispatch(authAction.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logOutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
