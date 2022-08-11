c6d1de3ac6552bd77d69c6a0d00b892f7690cc61
puzzlestudio1 <ali.fam0079@gmail.com>
2022-08-11 21:15:48 +0430

    (19) Talk to server - useEffect + ℹ️

--- 11th - advanced redux/src/App.js
+++ 11th - advanced redux/src/App.js
@@ -1,3 +1,4 @@
+import {useEffect} from "react";
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,25 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  /*
+  * Approach 2:
+  * 1. updating the redux store as usual
+  * 2. send data to server.
+  * transforming data logic stays in reducers.
+  * */
+
+
+  // Setting a subscription to the redux store.
+  // when cart data changes, useSelector will make this component to rerender and show the latest cart data.
+  const cart = useSelector((state) => state.cart);
+    // useEffect sends the latest cart data to backend each time cart changes.
+  useEffect(() => {
+      fetch('https://react-course-4b234-default-rtdb.europe-west1.firebasedatabase.app/cart.json',{
+            method: 'PUT',
+            body: JSON.stringify(cart)
+      } )
+  }, [cart]);
 
   return (
     <Layout>
--- 11th - advanced redux/src/components/Shop/ProductItem.js
+++ 11th - advanced redux/src/components/Shop/ProductItem.js
@@ -1,55 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import { cartActions } from '../../store/cart-slice';
+import { useDispatch,} from 'react-redux';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
+import {cartActions} from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-    // 1
-    const cart = useSelector((state) => state.cart);
     const dispatch = useDispatch();
 
     const { title, price, description, id } = props;
 
-    // Steps:
-    // 1. get state from redux
-    // 2. create brand-new state with the updated information.
-    // 3. replace new state with old state with replaceCart() reducer function.
-    // 4. send http request to the server.
-    // Problem: transforming cart data is done in component function and redux just saves the data. (not optimal)
     const addToCartHandler = () => {
-        // 2
-        const newTotalQuantity = cart.totalQuantity + 1;
-        // create copy via slice to avoid mutating original state (ONE MUST NEVER MUTATE THE REDUX STATE. EVER.)
-        const updatedItems = cart.items.slice();
-        const existingItem = updatedItems.find((item) => item.id === id);
-        if (existingItem) {
-            const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
-            updatedItem.quantity++;
-            updatedItem.totalPrice = updatedItem.totalPrice + price;
-            const existingItemIndex = updatedItems.findIndex(
-                (item) => item.id === id
-            );
-            updatedItems[existingItemIndex] = updatedItem;
-        } else {
-            updatedItems.push({
-                id: id,
-                price: price,
-                quantity: 1,
-                totalPrice: price,
-                name: title,
-            });
-        }
-
-        const newCart = {
-            totalQuantity: newTotalQuantity,
-            items: updatedItems,
-        };
-        // 3
-        dispatch(cartActions.replaceCart(newCart));
-        // 4
-        fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
-
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+    dispatch(cartActions.addItemToCart({
+        id,title,price
+    }));
     };
 
     return (
