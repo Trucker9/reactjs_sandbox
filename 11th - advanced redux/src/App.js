ab49d18e267abb517545c9bf6be28d8442b79d86
puzzlestudio1 <ali.fam0079@gmail.com>
2022-08-16 16:24:01 +0430

    (19) Using Action creators for side effects (sending data) + ℹ️

--- 11th - advanced redux/src/App.js
+++ 11th - advanced redux/src/App.js
@@ -4,8 +4,9 @@ import {useDispatch, useSelector} from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {uiActions} from "./store/ui-slice";
+
 import Notification from "./components/UI/Nitification";
+import {sendCartData} from "./store/cart-slice";
 
 // this variable will be created when the file is compiled.(not with each component render)
 let isInitial = true;
@@ -16,46 +17,15 @@ function App() {
     const notification = useSelector((state) => state.ui.notification);
 
     const cart = useSelector((state) => state.cart);
-    // Functions passed to useEffect must return a clean-up function or nothing. we can't use async functions.(they
-    // return promises)
+
     useEffect(() => {
-            const sendCartData = async () => {
-                dispatch(uiActions.setNotification({
-                    status: 'pending',
-                    title: 'Sending ...',
-                    message: 'Please wait to send cart data'
-                }));
-                const response = await fetch('https://react-course-4b234-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
-                    method: 'PUT',
-                    body: JSON.stringify(cart)
-                });
 
-                if (!response.ok) {
-                    throw new Error('Sending cart data failed!');
-                }
-                dispatch(uiActions.setNotification({
-                    status: 'success',
-                    title: 'Success!',
-                    message: 'done'
-                }));
-                // const responseData = await response.json(); we dont need it though.
-            }
-
-            // prevent sending cart data on first render (app is being created)
             if (isInitial) {
                 isInitial = false;
                 return;
             }
-
-            sendCartData().catch(err => {
-                dispatch(uiActions.setNotification({
-                    status: 'error',
-                    title: 'Error! ...',
-                    message: 'sending failed.'
-                }));
-                console.log(err);
-            })
-        }, // dispatch won't change (it's a redux function)
+            dispatch(sendCartData(cart));
+        },
         [cart, dispatch]);
 
     return (
--- 11th - advanced redux/src/store/cart-slice.js
+++ 11th - advanced redux/src/store/cart-slice.js
@@ -1,45 +1,105 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
+import {uiActions} from "./ui-slice";
 
 const cartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [],
-    totalQuantity: 0,
-  },
-  reducers: {
-    addItemToCart(state, action) {
-      const newItem = action.payload;
-      const existingItem = state.items.find((item) => item.id === newItem.id);
-      state.totalQuantity++;
-      if (!existingItem) {
-        state.items.push({
-          id: newItem.id,
-          price: newItem.price,
-          quantity: 1,
-          totalPrice: newItem.price,
-          name: newItem.title
-        });
-      } else {
-        existingItem.quantity++;
-        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
-      }
+    name: 'cart',
+    initialState: {
+        items: [],
+        totalQuantity: 0,
     },
-    removeItemFromCart(state, action) {
-      const id = action.payload;
-      const existingItem = state.items.find(item => item.id === id);
-      state.totalQuantity--;
-      if (existingItem.quantity === 1) {
-        state.items = state.items.filter(item => item.id !== id);
-      } else {
-        existingItem.quantity--;
-      }
+    reducers: {
+        addItemToCart(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find((item) => item.id === newItem.id);
+            state.totalQuantity++;
+            if (!existingItem) {
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title
+                });
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+            }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            state.totalQuantity--;
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+            }
+        },
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        }
     },
-    replaceCart(state, action) {
-      state.totalQuantity = action.payload.totalQuantity;
-      state.items = action.payload.items;
-    }
-  },
 });
+
+// WE NEED REDUCERS TO DO SOME SIDE EFFECTS AND THEN PERFORM SOME ACTIONS. WE USE ACTION CREATORS.
+
+// we are going to use this pattern. (using Action Creators)
+// first we do some work then we dispatch our action.
+//
+// const actionCreatorFunc = (args) => {
+//
+//     --What we want to do--
+//
+//     return function (dispatch) {
+//         dispatch( --actual action--);
+//     }
+// };
+
+
+// Later we call this function in "useEffect() of app.js"
+// basically in app.js we call actions, which return action objects for the store to do some work.
+// but with help of redux toolkit, we call a function that returns another function. then we are allowed to
+// perform some side effects.  it gets the dispatch function as an argument. so we can dispatch our actual action.
+export const sendCartData = (cart) => {
+
+    return async function (dispatch) {
+        dispatch(uiActions.setNotification({
+            status: 'pending',
+            title: 'Sending ...',
+            message: 'Please wait to send cart data'
+        }));
+
+        const sendReq = async () => {
+            const response = await fetch('https://react-course-4b234-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart)
+            });
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed!');
+            }
+        }
+        try {
+            await sendReq();
+        } catch (err) {
+            dispatch(uiActions.setNotification({
+                status: 'error',
+                title: 'Error! ...',
+                message: 'sending failed.'
+            }));
+        }
+
+
+        dispatch(uiActions.setNotification({
+            status: 'success',
+            title: 'Success!',
+            message: 'done'
+        }));
+
+    }
+};
+
 
 export const cartActions = cartSlice.actions;
 
