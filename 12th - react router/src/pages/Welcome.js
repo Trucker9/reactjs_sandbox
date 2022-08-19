7675e1ba46f7d7c137b65939122deba7fd00a5c2
puzzlestudio1 <ali.fam0079@gmail.com>
2022-08-19 19:25:25 +0430

    (20) nested routes

--- 12th - react router/src/pages/Welcome.js
+++ 12th - react router/src/pages/Welcome.js
@@ -1,7 +1,19 @@
+import {Route} from "react-router-dom";
+
+
 const Welcome = () => {
+
     return (
-        <h1>Welcome page</h1>
-    )
+        <section>
+            <h1>Welcome page</h1>
+            {/* If this component renders we are at "/welcome"
+            CONDITIONALLY we can render paragraph below if we are at "/welcome/userId" */
+            }
+            <Route path="/welcome/userId">
+                <p> user component</p>
+            </Route>
+        </section>
+    );
 }
 
 export default Welcome;
