65614d30b7e9b4eb698bf92f4f99606dd95f5c3d
puzzlestudio1 <ali.fam0079@gmail.com>
2022-08-23 01:29:10 +0430

    (20) redirect user without <Link> (programmatic navigation)

--- 13th - react router/src/pages/NewQuote.js
+++ 13th - react router/src/pages/NewQuote.js
@@ -1,8 +1,19 @@
 import QuoteForm from '../components/quotes/QuoteForm';
+import {useHistory} from "react-router-dom";
+
 
 const NewQuote = () => {
+
+    // we can use this hook to manipulate browser history.
+    // history.push(); & history.replace(); are functions to redirect the user. with push(); we can go back with back
+    // button
+    const history = useHistory();
+
     const addQuoteHandler = (quoteData) => {
+
+
         console.log(quoteData);
+        history.push('/quotes');
     };
 
     return (<QuoteForm onAddQuote={addQuoteHandler}/>);
