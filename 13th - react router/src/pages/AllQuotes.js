23fdf150254a2bd3f7db6876a2c628bdd5ae6f64
puzzlestudio1 <ali.fam0079@gmail.com>
2022-08-27 20:25:43 +0430

    (20) getting quotes from server

--- 13th - react router/src/pages/AllQuotes.js
+++ 13th - react router/src/pages/AllQuotes.js
@@ -1,12 +1,36 @@
-import QuoteList from '../components/quotes/QuoteList';
-
-const DUMMY_QUOTES = [
-    {id: 'q1', author: 'Max', text: 'Learning React is fun!'},
-    {id: 'q2', author: 'Maximilian', text: 'Learning React is great!'},
-];
+import QuoteList from "../components/quotes/QuoteList";
+import useHttp from "../hooks/use-http";
+import {getAllQuotes} from "../lib/api";
+import {useEffect} from "react";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
 const AllQuotes = () => {
-    return <QuoteList quotes={DUMMY_QUOTES}/>
+    const {
+        sendRequest,
+        status,
+        data: loadedQuotesArr,
+        err,
+    } = useHttp(getAllQuotes, true);
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
+
+    if (status === 'pending') {
+        return <div className={'centered'}>
+            <LoadingSpinner/>
+        </div>
+    }
+    if (err) {
+        return <div className={'centered focused'}>
+            {err}
+        </div>
+    }
+    if (status === 'completed' && loadedQuotesArr.length === 0) {
+        return <NoQuotesFound/>
+    }
+    return <QuoteList quotes={loadedQuotesArr}/>
 };
 
 export default AllQuotes;
