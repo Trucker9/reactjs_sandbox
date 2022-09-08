481a55be4f516dd15adc4778a85eb30d0cb69698
Ali MoshkinFaam <ali.fam0079@gmail.com>
2022-09-09 01:10:25 +0430

    (21) Optimizing with memo and lazy

--- 13th - react router + deploy/src/App.js
+++ 13th - react router + deploy/src/App.js
@@ -1,39 +1,50 @@
-import {Route, Switch, Redirect} from "react-router-dom";
+// After Testing, we need to optimize our code. (memo + lazy)
+// memo: learned previously
+// lazy: here
 
+
+import {Route, Switch, Redirect} from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+import React, {Suspense} from "react";
+
+// lazy, split code into chunks and load(download) them on demand. (only when needed)
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
 
 function App() {
     return (
         <div>
             <Layout>
-                <Switch>
-                    <Route exact path="/quotes">
-                        <AllQuotes/>
-                    </Route>
+                <Suspense fallback={<div>--Add spinner here--</div>}>
 
-                    <Route path="/quotes/:quoteId">
-                        <QuoteDetail/>
-                    </Route>
+                    <Switch>
+                        <Route exact path="/quotes">
+                            <AllQuotes/>
+                        </Route>
 
-                    <Route path="/new-quote">
-                        <NewQuote/>
-                    </Route>
+                        <Route path="/quotes/:quoteId">
+                            <QuoteDetail/>
+                        </Route>
 
-                    <Route exact path="/">
-                        <Redirect to="/quotes"/>
-                    </Route>
-                    {/*
+                        <Route path="/new-quote">
+                            <NewQuote/>
+                        </Route>
+
+                        <Route exact path="/">
+                            <Redirect to="/quotes"/>
+                        </Route>
+                        {/*
                     Anything can be a match with "*". but hence we are using switch, if it didn't match till the end,
                     our not found page will be rendered.
                     */}
-                    <Route path={'*'}>
-                        <NotFound/>
-                    </Route>
-                </Switch>
+                        <Route path={'*'}>
+                            <NotFound/>
+                        </Route>
+                    </Switch>
+                </Suspense>
             </Layout>
         </div>
     );
