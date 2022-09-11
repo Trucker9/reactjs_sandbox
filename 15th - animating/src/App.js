ddcf2a3fef189621bfa944137c7d36fa2910f36b
Ali MoshkinFaam <ali.fam0079@gmail.com>
2022-09-11 13:21:45 +0430

    (24) ❌ __CANCELLED__ class based...

--- 15th - animating/src/App.js
+++ 15th - animating/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import {Transition} from "react-transition-group";
 
 class App extends Component {
     state = {
@@ -24,6 +25,32 @@ class App extends Component {
             <div className="App">
                 <button className={"Button"} onClick={this.showModal}>Open Modal</button>
                 <h1>React Animations</h1>
+                <button
+                    className="Button"
+                    onClick={() =>
+                        this.setState(prevState => ({showBlock: !prevState.showBlock}))}
+                >
+                    Toggle
+                </button>
+                <br/>
+                <Transition
+                    in={this.state.showBlock}
+                    timeout={1000}
+                    mountOnEnter
+                    unmountOnExit>
+                    {state => (
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                width: 100,
+                                height: 100,
+                                margin: "auto",
+                                transition: 'opacity 1s ease-out',
+                                opacity: state === 'exiting' ? 0 : 1
+                            }}
+                        />
+                    )}
+                </Transition>
                 <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
                 <Backdrop show={this.state.modalIsOpen}/>
                 <h3>Animating Lists</h3>
